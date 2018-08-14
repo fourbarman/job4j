61d339de30413f08158edcf6e7f94ab65000b6b8
fourbarman <maks.java@yandex.ru>
2018-08-14 13:02:15 +0400

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,16 +4,25 @@ package ru.job4j.calculate;
 * Calculate.
 *
 * @author fourbarman (maks.java@yandex.ru)
+* @version $Id$
+* @since 0.1
 */
 public class Calculate{
-
 	/**
 	* Main.
 	*@param args - args.
 	*/
-	public static void main (String args[]){
-		
+	public static void main (String args[]){	
 		System.out.println("Hello World");	
+	}
+	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author fourbarman (mailto:maks.java@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
