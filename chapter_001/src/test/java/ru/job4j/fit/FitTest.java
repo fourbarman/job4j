43dc7b74bc29514ab79106f9baaa13488595894f
fourbarman <maks.java@yandex.ru>
2018-08-17 23:49:26 +0400

    3.3. Идеальный вес. Added JUnit tests

---
+++ chapter_001/src/test/java/ru/job4j/fit/FitTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.fit;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 17.08.2018
+ */
+public class FitTest {
+
+    /**
+     * Test manWeight.
+     */
+    @Test
+    public void whenManHeight180ThenWeightCloseTo92() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(92.0, 0.1));
+    }
+
+    /**
+     * Test womanWeight.
+     */
+    @Test
+    public void whenWomanHeight170ThenWeightCloseTo69() {
+        Fit fit = new Fit();
+        double weight = fit.womanWeight(170);
+        assertThat(weight, closeTo(69.0, 0.1));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/fit/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for human optimal weight task.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 17.08.2018
+ */
+
+package ru.job4j.fit;
