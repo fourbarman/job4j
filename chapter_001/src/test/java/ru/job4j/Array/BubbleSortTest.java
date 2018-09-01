e6512c1c1a1946f3902cf03c6470f72752c2adb4
fourbarman <maks.java@yandex.ru>
2018-09-01 16:24:21 +0400

    6.5. Array BubbleSort validated.

--- chapter_001/src/test/java/ru/job4j/Array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/Array/BubbleSortTest.java
@@ -17,7 +17,7 @@ public class BubbleSortTest {
      */
     @Test
     public void whenArrayIs54321ThenSorted12345() {
-        int[] array = new int[] {5 ,1 ,2 ,7 ,3};
+        int[] array = new int[] {5, 1, 2, 7, 3};
         BubbleSort bs = new BubbleSort();
         int[] expected = new int[] {1, 2, 3, 5, 7};
         int[] sorted = bs.sort(array);
@@ -28,7 +28,7 @@ public class BubbleSortTest {
      */
     @Test
     public void whenArrayIs9517382460ThenSorted12345() {
-        int[] array = new int[] {9 ,5 ,1 ,7 ,3, 8, 2 ,4, 6, 0};
+        int[] array = new int[] {9, 5, 1, 7, 3, 8, 2, 4, 6, 0};
         BubbleSort bs = new BubbleSort();
         int[] expected = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] sorted = bs.sort(array);
