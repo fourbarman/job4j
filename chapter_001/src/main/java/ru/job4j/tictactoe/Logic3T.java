0d204c17f2db03bb8449a2625ad1d158eb05e623
fourbarman <maks.java@yandex.ru>
2018-10-04 15:23:56 +0400

    Крестики-нолики на JavaFX. Alfa version. Fixed.

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -13,23 +13,20 @@ public class Logic3T {
     public boolean isWinnerX() {
         boolean result = false;
         //checking horisontals
-        for (int i = 0; i < this.table.length; i++) {
-                if (this.table[i][0].hasMarkX() & this.table[i][1].hasMarkX() & this.table[i][2].hasMarkX()) {
+        for (int i = 0; i < table.length; i++) {
+                if (table[i][0].hasMarkX() && table[i][1].hasMarkX() && table[i][2].hasMarkX()) {
                     result = true;
                     break;
             }
         }
         //checking diagonals
-        for (int i = 1; i <= this.table.length - 1; i++) {
-            if ((this.table[i - 1][i - 1].hasMarkX() == this.table[i][i].hasMarkX())
-                    || (this.table[i - 1][this.table.length - i].hasMarkX() == this.table[i][this.table.length - i - 1].hasMarkX())) {
-                result = true;
-                break;
+        if ((table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX())
+                || (table[0][2].hasMarkX() && table[1][1].hasMarkX() && table[2][0].hasMarkX()) ) {
+            result = true;
             }
-        }
         //checking verticals
-        for (int i = 0; i < this.table.length; i++) {
-            if (this.table[0][i].hasMarkX() && this.table[1][i].hasMarkX() && this.table[2][i].hasMarkX()) {
+        for (int i = 0; i < table.length; i++) {
+            if (table[0][i].hasMarkX() && table[1][i].hasMarkX() && table[2][i].hasMarkX()) {
                 result = true;
                 break;
             }
@@ -40,23 +37,20 @@ public class Logic3T {
     public boolean isWinnerO() {
         boolean result = false;
         //checking horisontals
-        for (int i = 0; i < this.table.length; i++) {
-            if (this.table[i][0].hasMarkO() && this.table[i][1].hasMarkO() && this.table[i][2].hasMarkO()) {
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][0].hasMarkO() && table[i][1].hasMarkO() && table[i][2].hasMarkO()) {
                 result = true;
                 break;
             }
         }
         //checking diagonals
-        for (int i = 1; i <= this.table.length - 1; i++) {
-            if ((this.table[i - 1][i - 1].hasMarkO() == this.table[i][i].hasMarkO())
-                    || (this.table[i - 1][this.table.length - i].hasMarkO() != this.table[i][this.table.length - i - 1].hasMarkO())) {
-                result = true;
-                break;
-            }
+        if ((table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO())
+                || (table[0][2].hasMarkO() && table[1][1].hasMarkO() && table[2][0].hasMarkO())) {
+            result = true;
         }
         //checking verticals
-        for (int i = 0; i < this.table.length; i++) {
-            if (this.table[0][i].hasMarkO() && this.table[1][i].hasMarkO() && this.table[2][i].hasMarkO()) {
+        for (int i = 0; i < table.length; i++) {
+            if (table[0][i].hasMarkO() && table[1][i].hasMarkO() && table[2][i].hasMarkO()) {
                 result = true;
                 break;
             }
@@ -66,9 +60,9 @@ public class Logic3T {
 
     public boolean hasGap() {
         boolean result = false;
-        for (int i = 0; i < this.table.length; i++) {
-            for (int j = 0; j < this.table.length; j++) {
-                if (!this.table[i][j].hasMarkX() || !this.table[i][j].hasMarkO()) {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (!table[i][j].hasMarkX() || !table[i][j].hasMarkO()) {
                     result = true;
                     break;
                 }
