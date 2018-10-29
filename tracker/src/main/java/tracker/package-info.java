d2dcfd90adb75ed8ddd7380dbe872eb14fa77030
fourbarman <maks.java@yandex.ru>
2018-10-29 11:31:50 +0400

    Tracker. Aggregation task. Base classes. v.0.2.

---
+++ tracker/src/main/java/tracker/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for Tracker task. OOP.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 29.10.2018
+ */
+package tracker;
--- tracker/src/main/java/tracker/start/Tracker.java
+++ tracker/src/main/java/tracker/start/Tracker.java
@@ -1,118 +1,129 @@
 package tracker.start;
 
 import tracker.models.*;
+
 import java.util.*;
 
 /**
  * Tracker.
  * Stores items.
+ *
  * @author fourbarman (maks.java@yandex.ru).
  * @version 1.
  * @since 26.10.2018.
  */
 public class Tracker {
-	private Item[] items = new Item[100];
-	private int position = 0;
-	private static final Random RN = new Random();
+    private Item[] items = new Item[100];
+    private int position = 0;
+    private static final Random RN = new Random();
 
     /**
      * Adds new Item to storage.
+     *
      * @param item Item to add.
      * @return Item.
      */
-	public Item add(Item item) {
-		item.setId(this.generateId());
-		this.items[position++] = item;
-		return item;
-	}
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[position++] = item;
+        return item;
+    }
 
     /**
      * Replaces old Item with new.
-     * @param id Id of new Item.
+     *
+     * @param id   Id of new Item.
      * @param item new Item to replace.
      */
-	public void replace(String id, Item item) {
+    public void replace(String id, Item item) {
         int index = -1;
         for (int i = 0; i != this.items.length; i++) {
             if (this.items[i] != null && this.items[i].getId().equals(id)) {
                 index = i;
             }
         }
-	    this.items[index] = item;
-	}
+        if (index != -1) {
+            this.items[index] = item;
+        }
+    }
 
     /**
      * Deletes the Item.
+     *
      * @param id Id of deleting Item.
      */
-	public void delete(String id) {
+    public void delete(String id) {
         int index = -1;
         for (int i = 0; i != this.items.length; i++) {
             if (this.items[i] != null && this.items[i].getId().equals(id)) {
                 index = i;
             }
         }
-		this.items[index] = null;
-		System.arraycopy(this.items, index + 1, this.items, index, this.items.length - index - 1);
-	}
+        this.items[index] = null;
+        System.arraycopy(this.items, index + 1, this.items, index, this.items.length - index - 1);
+    }
 
     /**
      * Gets all items.
+     *
      * @return array of items.
      */
-	public Item[] findAll() {
-		Item[] result = new Item[position];
-		for (int i = 0; i != this.position; i++) {
-			result[i] = this.items[i];
-		}
-		return result;
-	}
+    public Item[] findAll() {
+        Item[] result = new Item[position];
+        for (int i = 0; i != this.position; i++) {
+            result[i] = this.items[i];
+        }
+        return result;
+    }
 
     /**
      * Searches item by id.
+     *
      * @param id Id of item for searching.
      * @return Item.
      */
-	protected Item findById(String id) {
-		Item result = null;
-		for (Item item : items) {
-			if (item != null && item.getId().equals(id)) {
-				result = item;
-				break;
-			}
-		}
-		return result;
-	}
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
 
     /**
      * Searches item by keyword.
-	 * First loop searches number of items.
-	 * Second loop fills array to return.
+     * First loop searches number of items.
+     * Second loop fills array to return.
+     *
      * @param key Keyword for searching
      * @return Item.
      */
-	protected Item[] findByName(String key) {
-		int found = 0;
-		for (int i = 0; i != this.items.length; i++) {
-			if (items[i] != null && key != null && items[i].getName().contains(key)) {
-				found++;
-			}
-		}
-		Item[] result = new Item[found];
-		for (int i = 0; i != found; i++) {
-			if (items[i] != null && key != null && items[i].getName().contains(key)) {
-				result[i] = items[i];
-			}
-		}
-		return result;
-	}
+    public Item[] findByName(String key) {
+        int found = 0;
+        for (int i = 0; i != this.items.length; i++) {
+            if (items[i] != null && key != null && items[i].getName().contains(key)) {
+                found++;
+            }
+        }
+        Item[] result = new Item[found];
+        for (int i = 0; i != found; i++) {
+            if (items[i] != null && key != null && items[i].getName().contains(key)) {
+                result[i] = items[i];
+            }
+        }
+        return result;
+    }
 
     /**
      * Random generate id for item.
-	 * Generates with current time in millis and random int.
-	 * @return Id.
+     * Generates with current time in millis and random int.
+     *
+     * @return Id.
      */
-	String generateId() {
-		return String.valueOf(System.currentTimeMillis() + RN.nextInt());
-	}
+    String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
 }
