386e5b207037a09528435616c83b8694d9450a6a
fourbarman <maks.java@yandex.ru>
2019-08-01 10:52:44 +0400

    1.1 Котики обормотики. [#165452]

---
+++ chapter_002/src/main/java/cat/Cat.java
@@ -0,0 +1,19 @@
+package cat;
+/**
+ * Cat.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version 1.
+ * @since 01.08.2019.
+ */
+public class Cat {
+    /**
+     * Main.
+     *
+     * @param args String arguments.
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/cat/Dog.java
@@ -0,0 +1,20 @@
+package cat;
+/**
+ * Dog.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version 1.
+ * @since 01.08.2019.
+ */
+public class Dog {
+    /**
+     * Main.
+     *
+     * @param args String arguments.
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
