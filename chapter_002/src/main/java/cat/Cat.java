859d81562b568f6736d02b3cd57f12dc7f42283f
fourbarman <maks.java@yandex.ru>
2019-08-01 11:48:14 +0400

    1.5. Котенок гав и его котлета [#166083]

--- chapter_002/src/main/java/cat/Cat.java
+++ chapter_002/src/main/java/cat/Cat.java
@@ -7,13 +7,44 @@ package cat;
  * @since 01.08.2019.
  */
 public class Cat {
+    private String food;
+    private String name;
+    /**
+     * Gives nickname to a cat.
+     *
+     * @param nick nickname.
+     */
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+    /**
+     * Shows cat's nickname and food.
+     *
+     */
+    public void show() {
+        System.out.println(this.name + " ate " + this.food);
+    }
+    /**
+     * Cat's meal.
+     *
+     * @param meat Food.
+     */
+    public void eat(String meat) {
+        this.food = meat;
+    }
     /**
      * Main.
      *
      * @param args String arguments.
      */
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
