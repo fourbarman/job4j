ed213bbc850c8697f5bf813dfcf2e89348bad73c
fourbarman <maks.java@yandex.ru>
2019-08-05 15:44:45 +0400

    1.6. Петя и красная шапочка. [#166087]

---
+++ chapter_002/src/main/java/pioneer/Fox.java
@@ -0,0 +1,19 @@
+package pioneer;
+/**
+ * Fox.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version 1.
+ * @since 05.08.2019.
+ */
+
+public class Fox {
+    /**
+     * Eats the ball.
+     *
+     * @param ball Ball to eat.
+     */
+    public void tryEat(Ball ball){
+        System.out.println("Fox trying to eat the Ball");
+    }
+}
---
+++ chapter_002/src/main/java/pioneer/Hare.java
@@ -0,0 +1,19 @@
+package pioneer;
+/**
+ * Hare.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version 1.
+ * @since 05.08.2019.
+ */
+
+public class Hare {
+    /**
+     * Eats the ball.
+     *
+     * @param ball Ball to eat.
+     */
+    public void tryEat(Ball ball){
+        System.out.println("Hare trying to eat the Ball");
+    }
+}
