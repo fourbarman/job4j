38079108bfc8e973501a363383385fa85ce156ad
fourbarman <maks.java@yandex.ru>
2020-03-05 19:15:17 +0400

    Phone dictionary.

---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 05.11.2020
+ */
+public class PhoneDictionaryTest {
+    /**
+     * Test when found by name.
+     */
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    /**
+     * Test when found by surname.
+     */
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("rsen");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    /**
+     * Test when found by phone number.
+     */
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("487");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    /**
+     * Test when found by adress.
+     */
+    @Test
+    public void whenFindByAdress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("nsk");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+}
+
+
+
