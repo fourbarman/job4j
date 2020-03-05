113af377e5ba30b68480facc5fb3d9a9fc338b83
fourbarman <Puasson246>
2020-03-05 14:37:53 +0400

    ArrayList task #10094

--- chapter_003/src/main/java/ru/job4j/collection/UsageArrayList.java
+++ chapter_003/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -2,6 +2,13 @@ package ru.job4j.collection;
 
 import java.util.ArrayList;
 
+/**
+ * UsageArrayList.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version 1.
+ * @since 05.03.2020.
+ */
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
