4698c8e836b9c35a5e4988e097307ab81db97fe9
fourbarman <Puasson246>
2020-03-16 16:08:34 +0400

    Unique tasks

---
+++ chapter_003/src/test/java/ru/job4j/fullsearch/FullSearchTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.fullsearch;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+/**
+ * Test.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 16.03.2020
+ */
+public class FullSearchTest {
+    /**
+     * Test when 3 in than 2 unique out.
+     */
+    @Test
+    public void extractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(FullSearch.extractNumber(tasks), is(expected));
+    }
+}
