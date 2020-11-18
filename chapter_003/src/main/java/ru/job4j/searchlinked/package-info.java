bce967005a59456b75b994b4f0c93196f1e92ce9
fourbarman <42202792+fourbarman@users.noreply.github.com>
2020-11-18 14:52:42 +0400

    merge1

---
+++ chapter_003/src/main/java/ru/job4j/searchlinked/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for Linked List task.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 05.03.2020
+ */
+package ru.job4j.searchlinked;
---
+++ chapter_003/src/test/java/ru/job4j/searchlinked/PriorityQueueTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.searchlinked;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author fourbarman (mailto:maks.java@yandex.ru)
+ * @version 1
+ * @since 11.03.2020
+ */
+public class PriorityQueueTest {
+    /**
+     * Test priority queue.
+     */
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+}
